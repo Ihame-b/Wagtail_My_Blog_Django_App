fc680303ec1bdc4042bf682ecf65a61938fed38c
gihame <ihamegrbt@gmail.com>
2025-02-11 20:52:35 +0200

    adding more on blog

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -4,6 +4,7 @@ from django.db import models
 from wagtail.models import Page
 from wagtail.fields import RichTextField
 from wagtail.admin.panels import MultiFieldPanel, FieldPanel
+from wagtail.search import index
 
 class BlogIndexPage(Page):
     intro = RichTextField(blank=True)
@@ -12,3 +13,18 @@ class BlogIndexPage(Page):
         FieldPanel("intro", classname="full") 
         ]
 
+class BlogPage(Page):
+    date = models.DateField("Post date")
+    intro = models.CharField(max_length=250)
+    body = RichTextField(blank=True)
+
+    search_fields = Page.search_fields + [
+        index.SearchField('intro'),
+        index.SearchField('body'),
+    ]
+
+    content_panels = Page.content_panels + [
+        FieldPanel('date'),
+        FieldPanel('intro'),
+        FieldPanel('body', classname="full"),
+    ]
